7a7b482e22a12b4852bf44c266229887f0d63b0e
marcogulino-mg <marcogulino98@gmail.com>
2025-05-21 17:39:58 +0200

    feat:   NumberList.java:     - add array of integers and an int save index Attributes     - add Constructors/Concrete Methods
      Main.java:
        - add an array of integers and an instance of NumberList Class
        - test of getElementoSuccessivo Method

--- src/org/java/ciclabile/App.java
+++ src/org/java/ciclabile/App.java
@@ -2,6 +2,14 @@ package org.java.ciclabile;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Context: Attributes
+        int[] numbers = { 10, 22, 35 };
+        NumberList elencoNumeri = new NumberList(numbers);
+
+        // Context: NumberList Methods callback
+        // Note: getElementSuccessivo
+        System.out.println(elencoNumeri.getElementoSuccessivo());
+        System.out.println(elencoNumeri.getElementoSuccessivo());
+        System.out.println(elencoNumeri.getElementoSuccessivo());
     }
 }
--- src/org/java/ciclabile/NumberList.java
+++ src/org/java/ciclabile/NumberList.java
@@ -1,5 +1,20 @@
 package org.java.ciclabile;
 
 public class NumberList {
+    // Context: Attributes
+    private int[] numList;
+    private int arrIndex;
 
+    // Context: Methods
+    // Context: Constructors
+    public NumberList(int[] numList) {
+        this.numList = numList;
+    }
+
+    // Context: Concrete Methods
+    public int getElementoSuccessivo() {
+        int element = this.numList[arrIndex];
+        arrIndex++;
+        return element;
+    }
 }
